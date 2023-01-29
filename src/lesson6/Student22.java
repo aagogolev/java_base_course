186384c91007017a6f6fdfa51c9341ecf490897f
aleksandr.gogolev <aleksandr.gogolev@simbirsoft.com>
2023-01-29 22:54:52 +0300

    add homework lesson6

--- src/lesson6/Student22.java
+++ src/lesson6/Student22.java
@@ -8,7 +8,8 @@ public class Student22 {
     double average_math;
     double average_economy;
     double average_language;
-    Student22(int student_ID2, String name2, String surname2, int year_of_study2, double average_math2, double average_economy2, double average_language2){
+
+    Student22(int student_ID2, String name2, String surname2, int year_of_study2, double average_math2, double average_economy2, double average_language2) {
         student_ID = student_ID2;
         name = name2;
         surname = surname2;
@@ -17,12 +18,13 @@ public class Student22 {
         average_economy = average_economy2;
         average_language = average_language2;
     }
-    Student22(int student_ID3, String name3, String surname3, int year_of_study3){
+
+    Student22(int student_ID3, String name3, String surname3, int year_of_study3) {
         this(student_ID3, name3, surname3, year_of_study3, 0.0, 0.0, 0.0);
     }
 
-    Student22(){
-        this(0, null, null , 0, 0.0, 0.0, 0.0);
+    Student22() {
+        this(0, null, null, 0, 0.0, 0.0, 0.0);
     }
 }
 
@@ -36,7 +38,7 @@ class StudentTest22 {
 
         Student22 stud1 = new Student22(1, "Alex", "Below", 1984, 5.5, 6.3, 7.1);
 
-        Student22 stud2 = new Student22(2,"Ivan", "Ivanov", 1999);
+        Student22 stud2 = new Student22(2, "Ivan", "Ivanov", 1999);
         stud2.average_math = 3.3;
         stud2.average_economy = 4.4;
         stud2.average_language = 2.2;
